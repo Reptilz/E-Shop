6e1d1c475676923f2ddd24710c0fd5da86b2fe09
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-07-26 15:00:50 +0200

    first commit

---
+++ components/Nav.vue
@@ -0,0 +1,106 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+  <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <span class="font-semibold text-xl tracking-tight">StreetLoop</span>
+    </div>
+    <div class="block lg:hidden">
+      <button
+        class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+      >
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <NuxtLink
+          to="shop"
+          key="shop"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          Shop
+        </NuxtLink>
+        <NuxtLink
+          to="/about"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white ml-2"
+        >
+          About us
+        </NuxtLink>
+      </div>
+      <div>
+        <div class="pt-2 relative mx-auto text-gray-600">
+          <input
+            class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="search"
+            name="search"
+            placeholder="Search"
+          />
+          <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+            <svg
+              class="text-gray-600 h-4 w-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              id="Capa_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 56.966 56.966"
+              style="enable-background: new 0 0 56.966 56.966"
+              xml:space="preserve"
+              width="512px"
+              height="512px"
+            >
+              <path
+                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+    <NuxtLink
+      to="/bag"
+      key="bag"
+      class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 ml-20"
+    >
+      <img id="bag" src="../assets/medias/bag.png" alt="" />
+      <p>Bag(0)</p>
+    </NuxtLink>
+    <NuxtLink
+      to="/user"
+      key="user"
+      class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-12 ml-2"
+    >
+      <img
+        id="user-logo"
+        src="../assets/medias/abstract-user-flat-1.png"
+        alt=""
+      />
+      <p>Jordan</p>
+    </NuxtLink>
+  </nav>
+</template>
+
+<style scoped>
+#user-logo {
+  width: 40px;
+  height: 40px;
+}
+#bag {
+  width: 40px;
+  height: 40px;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="py-16 bg-white">
+    <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+      <div
+        class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12"
+      >
+        <div class="md:5/12 lg:w-5/12">
+          <img
+            src="../assets/medias/about.jpg"
+            alt="image"
+            loading="lazy"
+            width=""
+            height=""
+          />
+        </div>
+        <div class="md:7/12 lg:w-6/12">
+          <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">
+            The development of Sneakers is carried out by passionate creators
+          </h2>
+          <p class="mt-6 text-gray-600">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis
+            voluptatem accusantium nemo perspiciatis delectus atque autem!
+            Voluptatum tenetur beatae unde aperiam, repellat expedita
+            consequatur! Officiis id consequatur atque doloremque!
+          </p>
+          <p class="mt-4 mb-8 text-gray-600">
+            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
+            expedita at? Asperiores nemo possimus nesciunt dicta veniam
+            aspernatur quam mollitia.
+          </p>
+          <a href="/">
+            <button
+              type="button"
+              title="Start buying"
+              class="w-full py-3 px-6 text-center rounded-xl transition bg-gray-700 shadow-xl hover:bg-gray-600 active:bg-gray-700 focus:bg-gray-600 sm:w-max"
+            >
+              <span class="block text-white font-semibold">
+                Back <span>&#8635;</span></span
+              >
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "About",
+});
+</script>
+
+<style scoped></style>
